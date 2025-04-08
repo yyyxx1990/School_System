33b6fa8719a5319b562d34dc4c540582d81d7926
yyyxx1990 <937242343@qq.com>
2025-04-09 00:17:21 +0800

    第二次提交：添加限制查询集备注

--- qf_django_model/Two/views.py
+++ qf_django_model/Two/views.py
@@ -25,6 +25,11 @@ def get_user(response):
 
 def get_users(request):
     # 切片处理数据
+    # 限制查询集，可以使用下标的方法进行限制，等同于sql中limit
+    # 下标不能为负数
+    # 查询集的缓存：每个查询集都包含一个缓存，来最小化对数据库的访问
+    # 新建查询集，缓存首次为空 django会对第一次求值的数据做一个缓存，
+    # 并返回查询结构，以后的查询直接使用查询集的缓存。
     users = User.objects.all()[1:3]
 
     context = {
